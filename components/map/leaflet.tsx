e203224758b5aed552d1f9af68b61abe620ba3c5
Tran Trong Hien <tronghientran18@gmail.com>
2024-08-01 16:59:57 +0700

    [feat] - add map components

---
+++ components/map/leaflet.tsx
@@ -0,0 +1,196 @@
+"use client";
+
+import "leaflet/dist/leaflet.css";
+
+import { Box } from "@mantine/core";
+import { LatLngExpression } from "leaflet";
+import { useCallback, useMemo, useRef, useState } from "react";
+
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents
+} from "react-leaflet";
+
+interface ILeafletProps {
+  latitude: number;
+  longitude: number;
+}
+
+export const LeafletComponent = (props: ILeafletProps) => {
+  const { latitude, longitude } = props;
+  const [destination, setDestination] = useState<LatLngExpression>([
+    latitude,
+    longitude,
+  ]);
+
+  // const handleMapClick = (e: any) => {
+  //   const { lat, lng } = e.latlng;
+  //   alert(`Clicked at: ${lat}, ${lng}`);
+  // };
+
+  const MapPlaceholder = () => {
+    return (
+      <p>
+        Map of London.{" "}
+        <noscript>You need to enable JavaScript to see this map.</noscript>
+      </p>
+    );
+  };
+
+  return (
+    <Box className="min-h-screen">
+      <MapContainer
+        center={destination}
+        zoom={13}
+        className="min-h-[90vh]"
+        placeholder={<MapPlaceholder />}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* <MapEventsHandler handleMapClick={handleMapClick} /> */}
+        <DraggableMarker destination={destination} />
+      </MapContainer>
+    </Box>
+  );
+};
+
+const MapEventsHandler = ({
+  handleMapClick,
+}: {
+  handleMapClick: (e: any) => void;
+}) => {
+  useMapEvents({
+    click: (e) => handleMapClick(e),
+  });
+  return null;
+};
+
+function DraggableMarker({ destination }: { destination: any }) {
+  const [draggable, setDraggable] = useState(false);
+  const [position, setPosition] = useState(destination);
+
+  // Dragger marker
+  const markerRef = useRef<any>(null);
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current;
+        if (marker != null) {
+          setPosition(marker.getLatLng());
+        }
+      },
+    }),
+    []
+  );
+  const toggleDraggable = useCallback(() => {
+    setDraggable((d) => !d);
+  }, []);
+
+  // Event click
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return (
+    <Marker
+      draggable={draggable}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
+    >
+      <Popup minWidth={90}>
+        <span onClick={toggleDraggable}>
+          {draggable
+            ? "Marker is draggable"
+            : "Click here to make marker draggable"}
+        </span>
+      </Popup>
+    </Marker>
+  );
+}
+
+// const POSITION_CLASSES = {
+//   bottomleft: "leaflet-bottom leaflet-left",
+//   bottomright: "leaflet-bottom leaflet-right",
+//   topleft: "leaflet-top leaflet-left",
+//   topright: "leaflet-top leaflet-right",
+// };
+
+// const BOUNDS_STYLE = { weight: 1 };
+
+// function MinimapBounds({ parentMap, zoom }: { parentMap: Map; zoom: number }) {
+//   const minimap = useMap();
+
+//   // Clicking a point on the minimap sets the parent's map center
+//   const onClick = useCallback(
+//     (e) => {
+//       parentMap.setView(e.latlng, parentMap.getZoom());
+//     },
+//     [parentMap]
+//   );
+//   useMapEvent("click", onClick);
+
+//   // Keep track of bounds in state to trigger renders
+//   const [bounds, setBounds] = useState(parentMap.getBounds());
+//   const onChange = useCallback(() => {
+//     setBounds(parentMap.getBounds());
+//     // Update the minimap's view to match the parent map's center and zoom
+//     minimap.setView(parentMap.getCenter(), zoom);
+//   }, [minimap, parentMap, zoom]);
+
+//   // Listen to events on the parent map
+//   const handlers = useMemo(() => ({ move: onChange, zoom: onChange }), []);
+//   useEventHandlers({ instance: parentMap }, handlers);
+
+//   return <Rectangle bounds={bounds} pathOptions={BOUNDS_STYLE} />;
+// }
+
+// function MinimapControl({
+//   position,
+//   zoom,
+// }: {
+//   position: string;
+//   zoom: number;
+// }) {
+//   const parentMap = useMap();
+//   const mapZoom = zoom || 0;
+
+//   // Memoize the minimap so it's not affected by position changes
+//   const minimap = useMemo(
+//     () => (
+//       <MapContainer
+//         style={{ height: 80, width: 80 }}
+//         center={parentMap.getCenter()}
+//         zoom={mapZoom}
+//         dragging={false}
+//         doubleClickZoom={false}
+//         scrollWheelZoom={false}
+//         attributionControl={false}
+//         zoomControl={false}
+//       >
+//         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+//         <MinimapBounds parentMap={parentMap} zoom={mapZoom} />
+//       </MapContainer>
+//     ),
+//     []
+//   );
+
+//   const positionClass =
+//     (position && POSITION_CLASSES[position]) || POSITION_CLASSES.topright;
+//   return (
+//     <div className={positionClass}>
+//       <div className="leaflet-control leaflet-bar">{minimap}</div>
+//     </div>
+//   );
+// }
--- containers/hotel-search/components/map-filter.tsx
+++ containers/hotel-search/components/map-filter.tsx
@@ -1,26 +1,101 @@
 "use client";
 
-import { Box, Button, Drawer, Group, Radio, Title } from "@mantine/core";
+import { Box, Button, Drawer, Select } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { Map } from "lucide-react";
+import { Map, MapPinnedIcon } from "lucide-react";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+
+const LeafletComponent = dynamic(
+  () => import("@/components/map").then((mob) => mob.LeafletComponent),
+  { ssr: false }
+);
 
 export const MapFilter = () => {
   const [opened, { open, close }] = useDisclosure(false);
+  const [getCurrentUserLocation, setGetCurrentUserLocation] = useState({
+    latitude: 10.03,
+    longitude: 105.72,
+  });
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  function success(pos: { coords: any }) {
+    const crd = pos.coords;
+
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+
+    setGetCurrentUserLocation({
+      latitude: crd.latitude,
+      longitude: crd.longitude,
+    });
+  }
+
+  function errors(err: { code: any; message: any }) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          if (result.state === "granted") {
+            console.log(result.state);
+            navigator.geolocation.getCurrentPosition(success);
+            //If granted then you can directly call your function here
+          } else if (result.state === "prompt") {
+            console.log(result.state);
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "denied") {
+            //If denied then you have to show instructions to enable location
+          }
+          result.onchange = function () {
+            console.log(result.state);
+          };
+        });
+    } else {
+      alert("Sorry Not available!");
+    }
+  }, []);
 
   return (
     <Box>
-      <Drawer opened={opened} onClose={close} title="Sort by" position="bottom">
-        <Title order={2}>Sort by</Title>
+      <Drawer
+        opened={opened}
+        onClose={close}
+        size={"100%"}
+        title="Select hotel"
+        position="bottom"
+      >
+        <Select
+          data={[
+            "Can Tho",
+            "Sai Gon",
+            "Vinh Long",
+            "Tra Vinh",
+            "Soc Trang",
+            "Hau Giang",
+            "Tien Gian",
+            "Kien Giang",
+          ]}
+          leftSectionPointerEvents="none"
+          leftSection={<MapPinnedIcon />}
+          // error="Error"
+          placeholder="Please enter your hotel you want"
+          mb={10}
+        />
 
-        <Group mt="xs" className="flex flex-col gap-3">
-          <Radio value="react" label="Top picks for long stays" />
-          <Radio value="svelte" label="Price (lowest first)" />
-          <Radio value="ng" label="Property rating and price" />
-          <Radio value="vue" label="Best reviewd & lowest price" />
-          <Radio value="vue" label="Distance from downtown" />
-          <Radio value="vue" label="Top reviewed" />
-          <Radio value="vue" label="Homes & apartments first" />
-        </Group>
+        <LeafletComponent
+          latitude={getCurrentUserLocation.latitude}
+          longitude={getCurrentUserLocation.longitude}
+        />
       </Drawer>
 
       <Button onClick={open} variant="subtle" size="xs">
